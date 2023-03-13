851772e4e9027323085feeaeca58ce09fbfd7c41
Neon <58448036+NeonGamerBot-QK@users.noreply.github.com>
2023-03-13 19:32:56 -0400

    Init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+
+function App() {
+  return (
+<div className="hero min-h-screen bg-base-200">
+  <div className="hero-content text-center">
+    <div className="max-w-md">
+      <h1 className="text-5xl font-bold">Hello there</h1>
+      <p className="py-6">Coming soon</p>
+      <button className="btn btn-primary" onClick={() => window.location.replace("https://github.com/free-open-source-work/site")}>Github</button>
+    </div>
+  </div>
+</div>
+  );
+}
+
+export default App;
