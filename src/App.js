eb98047e2f258dec5bcb8875bb44550123b02a2f
Saahil <saahilattud@gmail.com>
2023-03-14 17:22:35 -0400

    Yes

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ function App() {
 <div className="hero min-h-screen bg-base-200">
   <div className="hero-content text-center">
     <div className="max-w-md">
-      <h1 className="text-5xl font-bold">Hello there</h1>
-      <p className="py-6">Coming soon</p>
-      <button className="btn btn-primary" onClick={() => window.location.replace("https://github.com/free-open-source-work/site")}>Github</button>
+      <h1 className="text-5xl font-bold">*.open-source.work</h1>
+      <p className="py-6"> Open source websites can be displayed here.</p>
+      <button className="btn btn-primary m-2" onClick={() => window.location.replace("https://github.com/free-open-source-work/site")}>Github</button>
+      <button className="btn btn-primary m-2" onClick={() => window.location.replace("https://github.com/free-open-source-work/register")}>Register</button>
     </div>
   </div>
 </div>
